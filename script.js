e2f2142bfdc0406cf6aa8bf9401cd0dde3b593fb
Ahmed-Arshad <ahmedarshadulhaque@hotmail.com>
2024-06-10 09:48:28 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// script.js
+document.getElementById('addTaskButton').addEventListener('click', function() {
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value.trim();
+    
+    if (taskText !== "") {
+        addTask(taskText);
+        taskInput.value = "";
+    }
+});
+
+function addTask(taskText) {
+    const taskList = document.getElementById('taskList');
+    
+    const li = document.createElement('li');
+    li.textContent = taskText;
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.classList.add('deleteButton');
+    deleteButton.addEventListener('click', function() {
+        taskList.removeChild(li);
+    });
+    
+    li.appendChild(deleteButton);
+    li.addEventListener('click', function() {
+        li.classList.toggle('completed');
+    });
+    
+    taskList.appendChild(li);
+}
